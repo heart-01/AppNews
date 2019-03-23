df9f47b086c92d70bffe9d2de6efc8a8fb0bc126
siwat jomewattana <s1104300051612@gmail.com>
2019-03-23 11:38:02 +0700

    ดึงข้อมูลjson API 3 หน้า

--- src/pages/movie/movie.ts
+++ src/pages/movie/movie.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Http } from '@angular/http'; //import เพื่อให้แอปเรียกใช้งานข้อมูลจากภายนอกเพจได้
+import 'rxjs/add/operator/map'; //import เพื่อให้แอปเรียกใช้งานข้อมูลจากภายนอกเพจได้
+
 /**
  * Generated class for the MoviePage page.
  *
@@ -15,7 +18,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class MoviePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  movnews:any=0; // สร้างตัวแปร movnews
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {   //เพิ่ม public http:Http เพื่อจะเรียกดูข้อมูลจากภายนอก
+    
+    this.http.get('https://www.reddit.com/r/popular/new.json?limit=20') //เรียกแสดงข้อมูลจาก url
+    .map(res=>res.json()) //ได้ขอมูลแล้วมาเก็บไว้ในรูปแบบ json
+    .subscribe(data=>{   
+      this.movnews = data.data.children; //data ตัวแรกคือ subscribe data ตัวที่ 2 คือโครงสร้างข้อมูลที่ในJSON API ส่วน children จะเป็นโครงสร้างที่อยู่ใน data อีกทีเพื่อดึงข้อมูลมา และนำผลลัพธ์จาก get จะนำมาใส่ตัวแปร movnews
+    });
+  
   }
 
   ionViewDidLoad() {
--- src/pages/science/science.ts
+++ src/pages/science/science.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Http } from '@angular/http'; //import เพื่อให้แอปเรียกใช้งานข้อมูลจากภายนอกเพจได้
+import 'rxjs/add/operator/map'; //import เพื่อให้แอปเรียกใช้งานข้อมูลจากภายนอกเพจได้
+
 /**
  * Generated class for the SciencePage page.
  *
@@ -15,7 +18,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SciencePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  science_news:any=0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http:Http) {   //เพิ่ม public http:Http เพื่อจะเรียกดูข้อมูลจากภายนอก
+  
+    this.http.get('https://www.reddit.com/r/science/new.json?limit=20') //เรียกแสดงข้อมูลจาก url
+    .map(res=>res.json()) //ได้ขอมูลแล้วมาเก็บไว้ในรูปแบบ json
+    .subscribe(data=>{   
+      this.science_news = data.data.children; //data ตัวแรกคือ subscribe data ตัวที่ 2 คือโครงสร้างข้อมูลที่ในJSON API ส่วน children จะเป็นโครงสร้างที่อยู่ใน data อีกทีเพื่อดึงข้อมูลมา และนำผลลัพธ์จาก get จะนำมาใส่ตัวแปร science_news
+    });
+  
   }
 
   ionViewDidLoad() {
